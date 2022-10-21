b255311064018d4ef87c3a922bf16de4f76d451a
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2022-10-20 22:05:04 -0500

    Thir classes begin

---
+++ ThirdCalss.md
@@ -0,0 +1,11 @@
+Order Operation
+
+1. the parenthesis
+
+For the following equation
+```
+ (3+6)*2 
+```
+
+- First We have to resolve the operation inside the parenthesis.
+    
---
+++ ThirdClass.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    // Declare int variable
+    int a = 32;
+    int b = 2;
+    int c = 4;
+
+    
+    return 0;
+}
